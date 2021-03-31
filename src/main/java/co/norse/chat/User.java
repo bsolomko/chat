4706b26a8129f29f27fdd8e7963c0ef686d49b76
mustafaev05 <shamil.mustafaev@norse.digital>
2021-03-31 17:22:03 +0300

    fixed first commit

--- src/main/java/co/norse/chat/User.java
+++ src/main/java/co/norse/chat/User.java
@@ -1,13 +1,17 @@
 package co.norse.chat;
 
+import java.util.Objects;
+
 public class User {
     private int id;
     private String username;
     private String email;
     private String firstName;
     private String lastName;
-    private String imageUser;
+    private String imageUrl;
     private String type;
+
+    public User() {}
 
     public User(int id, String username, String email, String firstName, String lastName, String imageUser, String type) {
         this.id = id;
@@ -15,10 +19,89 @@ public class User {
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.imageUser = imageUser;
+        this.imageUrl = imageUser;
         this.type = type;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(imageUrl, user.imageUrl) && Objects.equals(type, user.type);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, firstName, lastName, imageUrl, type);
+    }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
