6e003d80f227c8f7a5793dc6dda85cdd522a2d9c
Serhii Poliakov <serhii.poliakov@norse.digital>
2021-04-01 15:56:19 +0300

    Add clear chat, add repository for store message, add time for message

--- src/main/java/co/norse/chat/controller/MessageController.java
+++ src/main/java/co/norse/chat/controller/MessageController.java
@@ -1,25 +1,26 @@
 package co.norse.chat.controller;
 
 import co.norse.chat.model.Message;
+import co.norse.chat.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MessageController {
-    List<Message> allMessage = new ArrayList<>();
+    @Autowired
+    private MessageRepository messageRepository;
 
 
 @GetMapping("/chat")
     public String getHomePage(Model model,@ModelAttribute("newMessage") Message message){
-    model.addAttribute("messages",allMessage);
+    model.addAttribute("messages",messageRepository.getAllMessages());
     return "home";
 }
 
@@ -28,8 +29,15 @@ public class MessageController {
     Message _message = new Message();
     _message.setMessage(message.getMessage());
     _message.setAuthor(message.getAuthor());
-    allMessage.add(message);
+    _message.setCreateDataTime(message.getCreateDataTime());
+    _message.setId(message.getId());
+    messageRepository.addMessage(message);
     return "redirect:/chat";
+}
+@PostMapping("/deleteAllMessage")
+    public String deleteChat(){
+            messageRepository.deleteAllMessages();
+            return "redirect:/chat";
 }
 
 }
