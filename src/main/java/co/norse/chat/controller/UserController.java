8e40a37ddafea5c04dc71f592bf8d52288cafe5d
mustafaev05 <79975715+mustafaev05@users.noreply.github.com>
2021-04-16 15:29:14 +0300

    added UserController (#25)

---
+++ src/main/java/co/norse/chat/controller/UserController.java
@@ -0,0 +1,65 @@
+package co.norse.chat.controller;
+
+import co.norse.chat.User;
+import co.norse.chat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = "/users")
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user) {
+        User userModel= new User();
+        userModel.setFirstName(user.getFirstName());
+        userModel.setLastName(user.getLastName());
+        userModel.setEmail(user.getEmail());
+        userModel.setUsername(user.getUsername());
+        userModel.setImageUrl(user.getImageUrl());
+        userModel.setType(user.getType());
+        userService.addUser(userModel);
+        return userModel;
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity<User> getUserByID(@PathVariable(name = "id") long id) {
+        final User user = userService.findUserById(id);
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/users/byUsername")
+    public ResponseEntity<User> getUserByName(@RequestParam(name = "username") String username) {
+        final User user = userService.findUserByName(username);
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/users/byEmail")
+    public ResponseEntity<User> getUserByEmail(@RequestParam(name = "email") String email) {
+        final User user = userService.findUserByEmail(email);
+        return user != null
+                ? new ResponseEntity<>(user, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeUser(@PathVariable(name = "id") long id) {
+        userService.removeUser(id);
+    }
+
+}
