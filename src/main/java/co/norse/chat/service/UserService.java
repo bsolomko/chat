a2f860ecfbc881a0cf49cd6a653088f0dcf8b72a
mustafaev05 <shamil.mustafaev@norse.digital>
2021-04-30 17:21:00 +0300

    removed unnecessary lines

--- src/main/java/co/norse/chat/service/UserService.java
+++ src/main/java/co/norse/chat/service/UserService.java
@@ -27,29 +27,11 @@ public class UserService {
         user3.setPassword(passwordEncoder.encode(user3.getPassword()));
         User user4 = new User("sfa", "pass", "sfscm@gmail.com", "Sanchez", "Cold", "31221", "user");
         user4.setPassword(passwordEncoder.encode(user4.getPassword()));
-//        User user5 = new User("fas", "pass", "fas@gmail.com", "Fas", "Cold", "31221", "user");
-//        user5.setPassword("pass");
-//        User user6 = new User("sga", "pass", "sga@gmail.com", "Sam", "Cold", "31221", "user");
-//        user6.setPassword("pass");
-//        User user7 = new User("pom", "pass", "pom@gmail.com", "Perez", "Cold", "31221", "user");
-//        user7.setPassword("pass");
-//        User user8 = new User("leh", "pass", "leh@gmail.com", "Alex", "Cold", "31221", "user");
-//        user8.setPassword("pass");
-//        User user9 = new User("dim", "pass", "dim@gmail.com", "Dim", "Cold", "31221", "user");
-//        user9.setPassword("pass");
-////        User user10 = new User("fox", "pass", "fox@gmail.com", "Fox", "Cold", "31221", "user");
-//        user10.setPassword("pass");
 
         repository.addUser(user1);
         repository.addUser(user2);
         repository.addUser(user3);
         repository.addUser(user4);
-//        repository.addUser(user5);
-//        repository.addUser(user6);
-//        repository.addUser(user7);
-//        repository.addUser(user8);
-//        repository.addUser(user9);
-//        repository.addUser(user10);
     }
 
     public void addUser(User user) {
